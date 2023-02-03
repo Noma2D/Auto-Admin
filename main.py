38b24b260341beedbf1c158d2c6f97f0210b57e9
misha <misha131105@gmail.com>
2023-02-03 14:17:01 +0500

    v1.1 alfa with comment

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import os
 import re
 from time import sleep
 
+#показывает окно
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_MainWindow()
 ui.setupUi(MainWindow)
 MainWindow.show()
 
-
+#проверка ip адреса на правильность ввода
 def is_valid_ip(ip):
     pattern = re.compile(
         r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
@@ -19,21 +20,21 @@ def is_valid_ip(ip):
         return True
     return False
 
-
+#разворачивает ip для параметра DDNS
 def reverse_ip_address(ip_address):
     octets = ip_address.split('.')
     octlen = len(octets)
     octets = octets[:octlen-1]
     octets.reverse()
-
     return ".".join(octets)
 
-
+#Выводит последний октет ip адреса
 def last_digit_of_ip_address(ip_address):
     octets = ip_address.split('.')
     last_octet = octets[-1]
     return last_octet[-1]
 
+#записывает в файл введёные параметры для настройки DDNS
 def ddns_value():
     print("DDNS")
     file = open("values.txt", "w+")
@@ -48,6 +49,7 @@ def ddns_value():
     file.write("ranend=" + ui.DDNSranend.text() + "\n")
     sleep(5)
 
+#записывает в файл введёные параметры для настройки SSH по ключам
 def ssh_value():
     print("SSH")
     file = open("values.txt", "w+")
@@ -55,6 +57,7 @@ def ssh_value():
     file.write("uip=" + ui.SSHuip.text() + "\n")
     file.write("pass=" + ui.SSHpass.text() + "\n")
 
+#записывает в файл введёные параметры для настройки OpenVPN
 def vpn_value():
     print("VPN")
     file = open("values.txt", "w+")
@@ -63,6 +66,7 @@ def vpn_value():
     file.write("ip_mask=" + ui.VPNip_mask.text() + "\n")
     file.write("nic=" + ui.VPNnic.text() + "\n")
 
+#функция для вызова всплывающего окна если допущена ошибка в вводе ip адреса
 def ip_not_corecct():
     neval = QMessageBox()
     neval.setWindowTitle("неверный IP адрес")
@@ -70,6 +74,7 @@ def ip_not_corecct():
     neval.setIcon(QMessageBox.Warning)
     neval.exec_()
 
+#Параметры для настройки DDNS по умолчанию
 def ddns_umol():
         print("DDNS по умолчанию")
         file = open("values.txt", "w+")
@@ -82,29 +87,34 @@ def ddns_umol():
         file.write("ranstart=192.168.0.10\n")
         file.write("ranend=192.168.0.101\n")
 
+#Проверка ip адреса для DDNS
 def checking_ip_ddns():
     bol = is_valid_ip(ui.DDNSipnet.text()) & is_valid_ip(ui.DDNSmask.text()) & is_valid_ip(ui.DDNSserip.text()) & is_valid_ip(ui.DDNSranstart.text())\
                     & is_valid_ip(ui.DDNSranend.text())
     return bol
 
+#Проверка ip адреса для VPN
 def checking_ip_vpn():
     bolv = is_valid_ip(ui.VPNuip.text()) & is_valid_ip(ui.VPNip_mask)
     return bolv
 
+#Логистика при нажатии кнопки настроить
 def click():
+    #если выбран DDNS
     if ui.DDNS.isChecked():
+        #если не по умолчанию
         if ui.DDNSUmol.isChecked() == False:
             if checking_ip_ddns():
                 ddns_value()
                 os.system("./ddns.sh")
             else:
                 ip_not_corecct()
-
+        #если по умолчанию
         else:
             ddns_umol()
             os.system("./ddns.sh")
 
-
+    #если выбран SSH
     if ui.SSH.isChecked():
         if is_valid_ip(ui.SSHuip.text()):
             ssh_value()
@@ -112,6 +122,7 @@ def click():
         else:
             ip_not_corecct()
 
+    #если выбран VPN
     if ui.VPN.isChecked():
         if checking_ip_vpn():
             vpn_value()
@@ -119,17 +130,17 @@ def click():
         else:
             ip_not_corecct()
 
-
+    #если выбран Fire Wall
     if ui.FW.isChecked():
         print("FIreWall")
         os.system("./fire.sh")
 
-
+#Функция которая закрывает приложение если нажать кнопку отмена
 def can():
     sys.exit(app.exec_())
 
-
+#указывает какую функцию вызывать если нажата та или иная кнопка
 ui.setting.clicked.connect(click)
 ui.cancl.clicked.connect(can)
-
+#поддерживает программу включённой
 sys.exit(app.exec_())
