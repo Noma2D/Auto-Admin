391e5bb8f95de6aeebcaa260a168618659605acd
Noma2D <124347994+Noma2D@users.noreply.github.com>
2023-07-16 22:56:34 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 sudo mkdir /etc/auto-admin/
 sudo cp Auto-Admin/*.sh /etc/auto-admin
 sudo cp Auto-Admin/Auto-Admin /usr/bin/
-echo 'export PATH=\"\$HOME/bin:\$PATH\"' | tee -a ~/.bashrc > /dev/null
+
 sudo chmod +x /etc/auto-admin/*
 sudo chmod +x /usr/bin/Auto-Admin
-source ~/.bashrc
+
 
 if command -v Auto-Admin >/dev/null; then
     echo "Установка завершена"
